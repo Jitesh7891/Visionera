857fd354444c02d06e2c60feb2a92e46092fe451
Jitesh7891 <raj7000521814@gmail.com>
2025-02-10 20:35:29 +0530

    Add mobile navigation menu

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Hello this is homepage!
-      <UserButton/>
+    <div>
+      Hello this is homepage!
     </div>
   )
 }

01b991f15da1cd92d1501f4df4c18699667321b6
Jitesh7891 <raj7000521814@gmail.com>
2025-03-22 21:12:39 +0530

    Add Image Form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,39 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions'
+import { auth } from '@clerk/nextjs/server' 
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const AddTransformationPage = () => {
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = await auth() // Fetch the auth data on server-side
+
+  if (!userId) {
+    // console.log("hey")
+    redirect('/sign-in') // Redirect if no userId is found
+  }
+
+  const user = await getUserById(userId) // Fetch user data
+  const transformation = transformationTypes[type] // Get the transformation by type
+
   return (
-    <div>AddTransformationPage</div>
+    <>
+      <Header 
+        title={transformation.title} 
+        subtitle={transformation.subTitle} 
+      />
+    
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
-export default AddTransformationPage
+export default AddTransformationTypePage
